de9228d1ecc199f18847f1ef045d21a969d26ee5
mloser <zh975525512@gmail.com>
2022-06-23 17:01:00 +0800

    后端完善图片上传与下载

--- back/BluePeints/image_transfer.py
+++ back/BluePeints/image_transfer.py
@@ -1,7 +1,8 @@
+import flask
+import os
 from flask import Blueprint, request, jsonify
 from back.config import image_upload_path, image_download_path
-import os, flask
-
+from back.BluePeints.utils import generate_image_name
 
 bp = Blueprint('transfer', __name__, url_prefix='/api/image')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'JPG', 'PNG'}
@@ -11,18 +12,49 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1) in ALLOWED_EXTENSIONS
 
 
+# TODO: 传入要做的处理类型
 @bp.route('/upload', methods=['POST'])
 def upload_image():
     try:
-        img = request.files.get('photo')
-        file_path = os.path.join(image_upload_path, img.filename)
+        img = request.files.get('image')
+        file_extension = '.' + img.filename.split('.')[1]
+        new_file_name = generate_image_name() + file_extension
+        file_path = os.path.join(image_upload_path, new_file_name)
         img.save(file_path)
-        return jsonify({'message': 'success'})
+        return jsonify({'status': 'success', 'message': new_file_name})
     except Exception as err:
-        return jsonify({'message': str(err)})
+        return jsonify({'status': 'failed', 'message': str(err)})
 
 
+# TODO: 传出处理的类型
 @bp.route('/download', methods=['POST'])
 def download_image():
+    filename = os.path.join(image_download_path, request.get_json().get('filename'))
+    if os.path.exists(filename):
+        with open(filename, 'rb') as img:
+            resp = flask.make_response(img.read())
+            resp.headers['Content-Type'] = 'image/jpeg'
+            resp.headers['Process-Type'] = 'test'
+            return resp
+    else:
+        return jsonify({'status': 'wait', 'time': 5})
+
+
+# TODO: 根据处理类型调用服务
+@bp.route('/segment')
+def segment_image():
+    pass
+
+
+# TODO: 什么适合删除需要讨论
+@bp.route('/delete')
+def delete_image():
     filename = request.get_json().get('filename')
-    return flask.send_from_directory(image_download_path, filename)
+    try:
+        if os.path.exists(os.path.join(image_upload_path, filename)):
+            os.remove(os.path.join(image_upload_path, filename))
+        if os.path.exists(os.path.join(image_download_path, filename)):
+            os.remove(os.path.join(image_download_path, filename))
+        return jsonify({'status': 'success'})
+    except Exception as err:
+        return jsonify({'status': 'failed', 'message': str(err)})
--- back/run.py
+++ back/run.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
 from BluePeints.image_transfer import bp as transfer
-from BluePeints.image_processor import bp as processor
 
 app = Flask(__name__)
 
 app.register_blueprint(transfer)
-app.register_blueprint(processor)
 
 
 @app.route('/')
