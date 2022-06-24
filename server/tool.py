13f8fff334aaa8baa19f52127372f86040d1c499
CaoYX <1902818543@qq.com>
2022-06-24 10:44:59 +0800

    add img

--- server/tool.py
+++ server/tool.py
@@ -8,10 +8,6 @@ import numpy as np
 from PIL import Image
 import glob
 import cv2 as cv
-# 用于风格迁移
-import tensorflow as tf
-import tensorflow_hub as hub
-import matplotlib.pyplot as plt
 
 
 def img_metting(img_dir, mask_dir, metted_dir):
@@ -20,35 +16,25 @@ def img_metting(img_dir, mask_dir, metted_dir):
 
     for file in os.listdir(img_dir):
         pure_img_name = file.split('.')[-2]+'.png'
+
         if not os.path.exists(os.path.join(mask_dir, pure_img_name)):
             print("no exist" + os.path.join(mask_dir, pure_img_name))
             continue
+        print(pure_img_name+" is being met...")
         img = cv.imread(os.path.join(img_dir, file))
         mask = cv.imread(os.path.join(mask_dir, pure_img_name))
 
-        result = cv.bitwise_and(img, mask)
+        result = cv.bitwise_and(img, mask)          # 必须是相同通道数
         mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)  # 灰度图
         result = cv.cvtColor(result, cv.COLOR_BGR2BGRA)  # 4通道
 
         for i in range(0, result.shape[0]):  # 访问所有行
             for j in range(0, result.shape[1]):  # 访问所有列
                 if mask[i][j] < 100:
-                    result[i, j, 3] = 1
+                    result[i, j, 3] = 0
         cv.imwrite(os.path.join(metted_dir, pure_img_name), result)
         print(file+" is finished.")
 
-
-def test_png(img):
-    img = cv.imread(img)
-    img = cv.resize(img, (80, 100))
-    cv.imshow("small png", img)
-    cv.waitKey()
-    cv.destroyAllWindows()
-    print(img.shape)
-    # print(img[0])
-    # print(len(img[0]))
-    for i in range(30, 40):
-        print(img[51][i])
 
 def watercolor(img_dir, mask_dir , save_dir):
     if not os.path.exists(save_dir):
@@ -100,21 +86,51 @@ def oilpainting(img_dir, mask_dir , save_dir):
         cv.imwrite(os.path.join(save_dir, pure_img_name), result)
         print(file+" is finished.")
 
-def starrynight(content_image_path, style_image_path, save_dir):
-    content_image = plt.imread(content_image_path)
-    style_image = plt.imread(style_image_path)
-    content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.
-    style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.
-    style_image = tf.image.resize(style_image, (256, 256))
+# 用于修改背景
+def overlap(top_path, btm_path, save_dir):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    img_name = top_path.split('/')[-1]
+
+    top = cv.imread(top_path, cv.IMREAD_UNCHANGED)      # 读取四通道
+    btm = cv.imread(btm_path)
+    top_shape = top.shape
+    print("firstly, btm shape ")
+    print(btm.shape)
+    # cv.imshow("origin btm", btm)
+    btm_shape = (top_shape[1], top_shape[0])
+    btm = cv.resize(btm, btm_shape)
+    cv.imwrite(os.path.join(save_dir, "btm_resize.png"), btm)
+    # cv.imshow("resize", btm)
+    # cv.waitKey()
+    # cv.destroyAllWindows()
+    # btm_shape = btm.shape
+    print("top and btm shapes:")
+    print(top_shape)
+    print(btm.shape)
+    for i in range(0, top_shape[0]):  # 访问所有行
+        for j in range(0, top_shape[1]):  # 访问所有列
+            if (top[i][j][3] == 0):
+                top[i][j][:2] = btm[i][j][:2]
+                top[i][j][3] = 255
+
+    cv.imwrite(os.path.join(save_dir, img_name), top)
+
 
 if __name__ == "__main__":
     origin_dir = "test_data/test_images"
     mask_dir = "test_data/u2netp_results"
     save_dir = "test_data/mytest"
+    cat_path = r'E:\Code\u2_net\U-2-Net\test_data\mytest/metting/cat.png'
 
-    img_metting(origin_dir, mask_dir, "test_data/mytest/metting")
-    print("=======metting ok======")
-    watercolor(origin_dir, mask_dir, "test_data/mytest/watercolor")
-    print("=======watercolor ok======")
-    oilpainting(origin_dir, mask_dir, "test_data/mytest/oilpaint")
-    print("=======oilpaint ok======")
+    # img_metting(origin_dir, mask_dir, "test_data/mytest/metting")
+    # print("=======metting ok======")
+    # watercolor(origin_dir, mask_dir, "test_data/mytest/watercolor")
+    # print("=======watercolor ok======")
+    # oilpainting(origin_dir, mask_dir, "test_data/mytest/oilpaint")
+    # print("=======oilpaint ok======")
+
+    overlap(r"E:\Code\u2_net\U-2-Net\test_data\mytest/metting/cat.png", r"E:\Code\u2_net\U-2-Net\test_data/test_images/alask.png", save_dir)
+
+
